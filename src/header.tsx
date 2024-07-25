b3cc8e0185d266b021711c75cefbb479bc58e85b
shun-muratani-thecoo <os0039_muratani@thecoo.co.jp>
2024-07-25 15:54:16 +0900

    header作成

--- src/header.tsx
+++ src/header.tsx
@@ -1,19 +1,67 @@
 // src/Header.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FaClipboardList } from 'react-icons/fa';
+import './Header.scss';
 
-const Header: React.FC = () => {
-  return (
-    <header style={headerStyle}>
-      <h1>ToDo App</h1>
-    </header>
-  );
+// クラス名を定数として定義
+const HEADER_CLASS = 'header';
+const TRIGGER_CLASS = 'header-trigger';
+const VISIBLE_CLASS = 'header--visible';
+
+const useHeaderVisibility = (headerClass: string, triggerClass: string, visibleClass: string) => {
+  useEffect(() => {
+    const header = document.querySelector(`.${headerClass}`) as HTMLElement | null;
+    const trigger = document.querySelector(`.${triggerClass}`) as HTMLElement | null;
+
+    const showHeader = () => {
+      if (header) {
+        header.classList.add(visibleClass);
+      }
+    };
+
+    const hideHeader = () => {
+      if (header) {
+        header.classList.remove(visibleClass);
+      }
+    };
+
+    if (trigger) {
+      trigger.addEventListener('mouseover', showHeader);
+      header?.addEventListener('mouseleave', hideHeader);
+    }
+
+    return () => {
+      trigger?.removeEventListener('mouseover', showHeader);
+      header?.removeEventListener('mouseleave', hideHeader);
+    };
+  }, [headerClass, triggerClass, visibleClass]);
 };
 
-const headerStyle: React.CSSProperties = {
-  background: '#333',
-  color: '#fff',
-  textAlign: 'center',
-  padding: '10px'
+const Header: React.FC = () => {
+  useHeaderVisibility(HEADER_CLASS, TRIGGER_CLASS, VISIBLE_CLASS);
+
+  return (
+    <>
+      <div className={TRIGGER_CLASS}></div>
+      <header className={HEADER_CLASS}>
+        <div className="header__title">
+          <FaClipboardList className="header__icon" size={24} />
+          <h1>ToDo App</h1>
+        </div>
+        <ul className="header__nav-links">
+          <li className="header__nav-links__item">
+            <a href="/" className="header__nav-links__item__link">Home</a>
+          </li>
+          <li className="header__nav-links__item">
+            <a href="/about" className="header__nav-links__item__link">About</a>
+          </li>
+          <li className="header__nav-links__item">
+            <a href="/contact" className="header__nav-links__item__link">Contact</a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
 };
 
 export default Header;
