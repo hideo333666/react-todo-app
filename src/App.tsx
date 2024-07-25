9d2dca7f16a290d6aba66e7f2acd1bfec84611e3
shun-muratani-thecoo <os0039_muratani@thecoo.co.jp>
2024-07-25 16:58:50 +0900

    タスクドラッグ&ドロップ機能追加、タスク追加機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // src/App.tsx
 import React from 'react';
 import Header from './header';
+import TodoApp from './components/Todo/Todo';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <TodoApp />
     </div>
   );
 };
